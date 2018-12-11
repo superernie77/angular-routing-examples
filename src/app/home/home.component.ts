67e0d34c753fe8f3328d51e93f1be8c8bff15605
superernie77 <superernie77@gmail.com>
2018-12-11 17:24:44 +0100

    added route paraneter and navigation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  // get router in constructor
+  constructor(private router : Router) { }
 
   ngOnInit() {
+  }
+
+  // use navigate to go somewhere
+  onLoadServers(){
+    this.router.navigate(['/servers']);
   }
 
 }
